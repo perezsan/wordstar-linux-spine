3098f77541b455a6761afab0641dddb74e1b4586
perezsan <perezsan@users.noreply.github.com>
2020-03-24 07:29:28 -0700

    Update install-spine.sh

--- install-spine.sh
+++ install-spine.sh
@@ -1,7 +1,8 @@
-#~/bin/bash
+#!/bin/bash
 chmod 700 spine/* \
 && chmod 700 spine/reticulation/install-* \
 && chmod 700 spine/reticulation/reopen-* \
 && chmod 700 spine/reticulation/set-caps-* \
 && chmod 700 spine/reticulation/wsl-setup* \
 && chmod 700 spine/reticulation/close-* \
+&& sh -c spine/reticulation/wsl-setup
