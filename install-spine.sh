5a54bc5936d2ee2fdb94e1f191835b398da59937
perezsan <perezsan@users.noreply.github.com>
2020-03-24 07:07:46 -0700

    Add files via upload
    ghetto install-in-place script. will make the proper files in reticulation folder executable after downloading from git, although most of these files I've marked executable seem to be only needed for my Wordstar Linux Manjaro distro I'm concurrently working on.

---
+++ install-spine.sh
@@ -0,0 +1,7 @@
+#~/bin/bash
+chmod 700 spine/* \
+&& chmod 700 spine/reticulation/install-* \
+&& chmod 700 spine/reticulation/reopen-* \
+&& chmod 700 spine/reticulation/set-caps-* \
+&& chmod 700 spine/reticulation/wsl-setup* \
+&& chmod 700 spine/reticulation/close-* \
