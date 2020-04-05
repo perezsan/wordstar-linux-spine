9abe8f8533a617ae17cb38c152e0ea24f55afd3b
Your Name <your@email.addy>
2020-04-05 02:34:52 -0700

    modified:   install-spine.sh 	modified:   spine/reticulation/wsl-setup 	deleted:    spine/wsl-change-spine-dir-python 	copy-spine-default-dir.sh

--- install-spine.sh
+++ install-spine.sh
@@ -1,8 +1,49 @@
 #!/bin/bash
-chmod 700 spine/* \
-&& chmod 700 spine/reticulation/install-* \
-&& chmod 700 spine/reticulation/reopen-* \
-&& chmod 700 spine/reticulation/set-caps-* \
-&& chmod 700 spine/reticulation/wsl-setup* \
-&& chmod 700 spine/reticulation/close-* \
-#&& sh -c spine/reticulation/wsl-setup
+
+bashrc_check() {
+if ls ~/.bashrc 1> /dev/null 2>&1; then
+	echo 'export SPINE_DIR="$HOME/.config/spine"' >> ~/.bashrc \
+	&& echo 'export PATH="$PATH:$SPINE_DIR"' >> ~/.bashrc
+else
+	touch ~/.bashrc \
+	&& echo 'export SPINE_DIR="$HOME/.config/spine"' >> ~/.bashrc \
+	&& echo 'export PATH="$PATH:$SPINE_DIR"' >> ~/.bashrc
+fi
+}
+
+profile_check() {
+if ls ~/.profile 1> /dev/null 2>&1; then
+	echo 'export SPINE_DIR="$HOME/.config/spine"' >> ~/.profile \
+	&& echo 'export PATH="$PATH:$SPINE_DIR"' >> ~/.profile
+else
+	touch ~/.profile \
+	&& echo 'export SPINE_DIR="$HOME/.config/spine"' >> ~/.profile \
+	&& echo 'export PATH="$PATH:$SPINE_DIR"' >> ~/.profile
+fi
+}
+
+bash_profile_check() {
+if ls ~/.bash_profile 1> /dev/null 2>&1; then
+	echo 'export SPINE_DIR="$HOME/.config/spine"' >> ~/.bash_profile \
+	&& echo 'export PATH="$PATH:$SPINE_DIR"' >> ~/.bash_profile
+else
+	touch ~/.bash_profile \
+	&& echo 'export SPINE_DIR="$HOME/.config/spine"' >> ~/.bash_profile \
+	&& echo 'export PATH="$PATH:$SPINE_DIR"' >> ~/.bash_profile
+fi
+}
+
+if [[ -v SPINE_DIR ]]; then
+	echo "Spine enviroment is already set to $SPINE_DIR"
+else
+	echo "Adding SPINE_DIR to ~/.profile ~/.bash_profile and ~/.bashrc" \
+	&& profile_check \
+	&& bash_profile_check \
+	&& bashrc_check
+fi
+
+copy_spine() {
+exec bash -c ./copy-spine-default-dir.sh
+}
+
+copy_spine
