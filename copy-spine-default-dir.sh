bb68a7da1cd5bdd3596548bc28e7804f5093bbe1
Your Name <your@email.addy>
2020-04-08 19:44:16 -0700

    modified:   copy-spine-default-dir.sh 	modified:   spine/assets/meta.xml 	spine/assets/reference.odt 	spine/assets/styles.xml

--- copy-spine-default-dir.sh
+++ copy-spine-default-dir.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 source ~/.bashrc; \
 cp -axr spine "$HOME/.config" \
-&& ~/.config/spine/reticulation/wsl-setup
+&& ~/.config/spine/reticulation/wsl-setup;
+mkdir -p ~/.pandoc;
+if ls ~/.pandoc/reference.odt 1> /dev/null 2>&1; then
+	echo "file reference.odt already exists .. you may have custom reference.odt file already in place. please copy ~/.config/spine/assets/reference.odt to ~/.pandoc if you want SPINE's default manuscript output"
+else
+	cp ~/.config/spine/assets/reference.odt ~/.pandoc/
+fi
 
