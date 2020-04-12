d5b687bbe1048b813941b60a2b473b8bbd4e7d93
Your Name <your@email.addy>
2020-04-11 19:26:26 -0700

    modified:   copy-spine-default-dir.sh 	modified:   spine/reticulation/change-odt-meta-title

--- copy-spine-default-dir.sh
+++ copy-spine-default-dir.sh
@@ -6,6 +6,7 @@ mkdir -p ~/.pandoc;
 if ls ~/.pandoc/reference.odt 1> /dev/null 2>&1; then
 	echo "file reference.odt already exists .. you may have custom reference.odt file already in place. please copy ~/.config/spine/assets/reference.odt to ~/.pandoc if you want SPINE's default manuscript output"
 else
-	cp ~/.config/spine/assets/reference.odt ~/.pandoc/
+	cp ~/.config/spine/assets/reference.odt ~/.pandoc/ \
+	&& cp ~/.config/spine/assets/*.xml ~/.pandoc/
 fi
 
