a6f2d6b7691d7fb96a922dde40fb1b1bf5aea2b8
perezsan <perezsan@users.noreply.github.com>
2020-05-03 03:05:19 -0700

    Update copy-spine-default-dir.sh

--- copy-spine-default-dir.sh
+++ copy-spine-default-dir.sh
@@ -4,7 +4,8 @@ cp -axrf spine "$HOME/.config" \
 && ~/.config/spine/reticulation/wsl-setup;
 mkdir -p ~/.pandoc;
 if ls ~/.pandoc/reference.odt 1> /dev/null 2>&1; then
-	echo "file reference.odt already exists .. you may have custom reference.odt file already in place. please copy ~/.config/spine/assets/reference.odt to ~/.pandoc if you want SPINE's default manuscript output"
+	echo "~/.pandoc/reference.odt already exists .. you may have custom reference.odt already in place. Please copy ~/.config/spine/assets/reference.odt to ~/.pandoc if you want SPINE's standar manuscript format output" \
+	&& cp ~/.config/spine/assets/*.xml ~/.pandoc/
 else
 	cp ~/.config/spine/assets/reference.odt ~/.pandoc/ \
 	&& cp ~/.config/spine/assets/*.xml ~/.pandoc/
